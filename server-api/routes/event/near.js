f8a88f55fdd5ebc789677b7ca64311a16542c1bc
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-09 17:29:31 +0300

    Near events endpoint is updated.

--- server-api/routes/event/near.js
+++ server-api/routes/event/near.js
@@ -1,11 +1,17 @@
 const db = require('../../db-config');
+const moment = require('moment');
 
 module.exports = server => {
-    server.get('/api/event/near/:time', (req, res) => {
+    server.get('/api/event/near/:time/', (req, res) => {
         let time = req.params.time;
-        db.query('SELECT * FROM `EventCard` WHERE time = ?', [time])
+        time = time + '%';
+        db.query('SELECT * FROM `EventCard` WHERE time LIKE ?', [time])
             .then(data => {
-                console.log(data);
+                data.forEach(element => {
+                    let time = element.time;
+                    element.date = moment(time).format('DD/MM/YYYY');
+                    element.time = moment(time).format('HH:mm');
+                });
                 res.send(data);
             })
             .catch(error => {
