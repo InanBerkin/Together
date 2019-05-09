c134a1802bfc172d5cd11481de31c3130f25a214
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-09 18:16:59 +0300

    Event search endpoint is fixed.

--- server-api/routes/city/search.js
+++ server-api/routes/city/search.js
@@ -2,15 +2,19 @@ const db = require('../../db-config');
 
 module.exports = server => {
     server.get('/api/city/search/:text/', (req, res) => {
-        let text = `%${req.params.text}%`;
-        db.query('SELECT name FROM `City` WHERE name LIKE ?', text)
-            .then(data => {
-                console.log(data);
-                res.send(data);
-            })
-            .catch(error => {
-                console.log(error);
-                res.send([]);
-            });
+        if (req.params.text.length > 0) {
+            const text = `%${req.params.text}%`;
+            db.query('SELECT name FROM `City` WHERE name LIKE ?', [text])
+                .then(data => {
+                    console.log(data);
+                    res.send(data);
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.send([]);
+                });
+        } else {
+            res.send([]);
+        }
     });
 };
