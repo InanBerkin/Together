7f57e0649f06690f89612ec9658394c0c9314bc7
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-19 15:30:09 +0300

    PM endpoints are added.

---
+++ server-api/routes/messaging/pm_between.js
@@ -0,0 +1,20 @@
+const db = require('../../db-config');
+
+module.exports = server => {
+    server.get('/api/messaging/pm/pages/:sender/:start/:end/', (req, res) => {
+        const receiver_id = req.user.id;
+        const sender_id = req.params.sender;
+        const start_id = req.params.start;
+        const end_id = req.params.end;
+
+        db.query('SELECT message_id, time, message_text FROM `pmcard` WHERE receiver_id = ? AND sender_id = ? AND (message_id BETWEEN ? AND ?)', [receiver_id, sender_id, start_id, end_id])
+            .then(data => {
+                console.log(data);
+                res.send(data);
+            })
+            .catch(error => {
+                console.log(error);
+                res.send(401);
+            });
+    });
+};
