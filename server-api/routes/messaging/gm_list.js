e0779c6f9801bb335b69f3e226ef8c1f8e8f790e
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-19 18:05:48 +0300

    GM endpoints are added.

--- server-api/routes/messaging/gm_list.js
+++ server-api/routes/messaging/gm_list.js
@@ -1,10 +1,10 @@
 const db = require('../../db-config');
 
 module.exports = server => {
-    server.get('/api/messaging/gm/list/', (req, res) => {
-        const receiver_id = req.user.id;
+    server.get('/api/messaging/gm/list/:group/', (req, res) => {
+        const group_id = req.params.group;
 
-        db.query('SELECT sender_id, sender_name, sender_image, last_mid, last_mtime, last_msg, is_self FROM `pmlist` WHERE receiver_id = ?', [receiver_id])
+        db.query('SELECT * FROM `gmlist` WHERE group_id = ?', [group_id])
             .then(data => {
                 console.log(data);
                 res.send(data);
