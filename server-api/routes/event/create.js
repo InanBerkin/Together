06f6cfd1ef7bd12524b6560a4822a539a6a4eef0
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-19 21:45:00 +0300

    City creation is added to event creation.

--- server-api/routes/event/create.js
+++ server-api/routes/event/create.js
@@ -1,7 +1,29 @@
 const db = require('../../db-config');
 
+async function createLocation(country, city) {
+    const country_exist = await db.query('SELECT * FROM `country` WHERE name = ?', [country]);
+
+    if (country_exist.length !== 1) {
+        await db.query('INSERT INTO `country` (name) VALUES (?)', [country]);
+        await db.query('INSERT INTO `city` (name, in_country) VALUES (?, (SELECT country_id FROM `country` ORDER BY country_id DESC LIMIT 1))', [city]);
+        const data = await db.query('(SELECT city_id FROM `city` ORDER BY city_id DESC LIMIT 1)');
+        return data[0].city_id;
+    } else {
+        const country_id = country_exist[0].country_id;
+        const city_exist = await db.query('SELECT * FROM `city` WHERE name = ? AND in_country = ?', [city, country_id]);
+
+        if (city_exist.length !== 1) {
+            await db.query('INSERT INTO `city` (name, in_country) VALUES (?, ?)', [city, country_id]);
+            const data = await db.query('(SELECT city_id FROM `city` ORDER BY city_id DESC LIMIT 1)');
+            return data[0].city_id;
+        } else {
+            return city_exist[0].city_id;
+        }
+    }
+}
+
 module.exports = server => {
-    server.post('/api/event/create/', (req, res) => {
+    server.post('/api/event/create/', async (req, res) => {
         const name = req.body.name;
         const description = req.body.description;
         const start_time = req.body.starttime;
@@ -10,12 +32,15 @@ module.exports = server => {
         const location_lng = req.body.locationlng;
         const quota = req.body.quota;
         const city = req.body.city;
+        const country = req.body.country;
         const organizers = req.body.organizers;
         const group_id = req.body.groupid;
         const image = req.body.image;
 
+        const city_id = await createLocation(country, city);
+
         db.query('START TRANSACTION').then(() => {
-            db.query('INSERT INTO `Event` (name, description, location_lat, location_lng, quota, start_time, end_time, group_event, event_in, image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?, (SELECT city_id FROM `city` WHERE `name` = ?), ?)', [name, description, location_lat, location_lng, quota, start_time, end_time, group_id, city, image])
+            db.query('INSERT INTO `Event` (name, description, location_lat, location_lng, quota, start_time, end_time, group_event, event_in, image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [name, description, location_lat, location_lng, quota, start_time, end_time, group_id, city_id, image])
                 .then(() => {
                     let q = 'INSERT INTO `attend` (account_id, event_id, status) VALUES ';
 
