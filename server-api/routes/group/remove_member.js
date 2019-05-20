85407eb6a1ef39e38542688ad80012e4d6919847
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-20 13:21:11 +0300

    Remove_member is fixed.

--- server-api/routes/group/remove_member.js
+++ server-api/routes/group/remove_member.js
@@ -8,7 +8,7 @@ module.exports = server => {
 
         const is_admin = await db.query('SELECT * FROM `member` WHERE group_id = ? AND account_id = ? AND status = 3', [group_id, account_id]);
 
-        if (is_admin === 1) {
+        if (is_admin.length === 1) {
             db.query('UPDATE `Member` SET status = -1 WHERE group_id = ? AND account_id = ?', [group_id, member_id])
                 .then(() => {
                     const msg = `User ${member_id} is removed from group ${group_id}.`;
