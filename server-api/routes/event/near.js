ffc1cec986fc01583b9a16df2dc80d75f47bd453
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-20 23:15:02 +0300

    Some changes made.

--- server-api/routes/event/near.js
+++ server-api/routes/event/near.js
@@ -2,11 +2,12 @@ const db = require('../../db-config');
 const moment = require('moment');
 
 module.exports = server => {
-    server.get('/api/event/near/:start_time/:end_time/', (req, res) => {
+    server.get('/api/event/near/:start_time/:end_time/:city/', (req, res) => {
         const start_time = req.params.start_time;
         const end_time = req.params.end_time;
+        const city = `%${req.params.city}%`;
 
-        db.query('SELECT * FROM `EventCard` WHERE (start_time BETWEEN ? AND ?) OR (end_time BETWEEN ? AND ?)', [start_time, end_time, start_time, end_time])
+        db.query('SELECT * FROM `EventCard` WHERE ((start_time BETWEEN ? AND ?) OR (end_time BETWEEN ? AND ?)) AND city LIKE ?', [start_time, end_time, start_time, end_time, city])
             .then(data => {
                 data.forEach(element => {
                     const time = element.time;
