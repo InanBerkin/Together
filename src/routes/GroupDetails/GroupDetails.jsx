3dc1ff2880939695f8e141118b5858977d519f8c
Zafer Tan ÇANKIRI <39600330+ztancankiri@users.noreply.github.com>
2019-05-21 07:50:35 +0300

    3dot removed.

--- src/routes/GroupDetails/GroupDetails.jsx
+++ src/routes/GroupDetails/GroupDetails.jsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container, Grid, Popup, Button, Divider, Comment, Form, Label, Header } from 'semantic-ui-react';
-import GroupDetailsSidebar from "components/side-bar/groupDetailsSidebar";
+import GroupDetailsSidebar from 'components/side-bar/groupDetailsSidebar';
 
 import Skeleton from 'react-loading-skeleton';
 import api from 'api.js';
 
-import "./GroupDetails.scss";
+import './GroupDetails.scss';
 
 function GroupDetails({ match }) {
-
     const [groupData, setGroupData] = useState();
     const [imageStyle, setImageStyle] = useState({});
     const [allMembers, setAllMembers] = useState([]);
@@ -24,17 +23,17 @@ function GroupDetails({ match }) {
         const image_path = api.getImage(data.image_path);
         const style = {
             backgroundImage: `url(${image_path})`
-        }
+        };
         setImageStyle(style);
         const members = await api.getAllGroupMembers(match.params.id);
         const memberStatus = await api.getMemberStatus(match.params.id);
         setAllMembers(members.data);
         setMemberStatus(memberStatus.data.status);
-    }
+    };
 
     function displayAllCategories() {
         if (!groupData) {
-            return <Skeleton width={500} />
+            return <Skeleton width={500} />;
         }
         const colors = ['yellow', 'blue', 'pink', 'red', 'green'];
         return groupData.categories.map((category, index) => {
@@ -49,43 +48,31 @@ function GroupDetails({ match }) {
     return (
         <div>
             <Grid>
-                <Grid.Column stretched width='3'>
+                <Grid.Column stretched width="3">
                     <GroupDetailsSidebar members={groupData.members} admins={groupData.admins} allMembers={allMembers} group_name={groupData.group_name} group_id={groupData.group_id} member_status={memberStatus} />
                 </Grid.Column>
-                <Grid.Column stretched width='13'>
+                <Grid.Column stretched width="13">
                     <Container>
                         <div className="group side-crop" style={imageStyle} />
                         <div className="group-info">
-                            <div className="host-name info-block">
-                            </div>
+                            <div className="host-name info-block" />
                             <div className="event-name info-block">
                                 <div>
                                     {groupData.group_name}
-                                    <br></br>
+                                    <br />
                                 </div>
-                                <Popup
-                                    trigger={<Button icon="ellipsis horizontal"></Button>}
-                                    content='Ban Group'
-                                    on='click'
-                                    position='bottom center'
-                                    hideOnScroll
-                                />
                             </div>
-                            <div className="categories">
-                                {displayAllCategories()}
-                            </div>
+                            <div className="categories">{displayAllCategories()}</div>
                             <div>
                                 <Header as="h3">Description</Header>
-                                <p>
-                                    {groupData.description || <Skeleton count={2} width={300} />}
-                                </p>
+                                <p>{groupData.description || <Skeleton count={2} width={300} />}</p>
                             </div>
                             <Divider />
                             <h2>Comments</h2>
                             <Comment.Group>
                                 <Form reply>
                                     <Form.TextArea />
-                                    <Button content='Add Comment' labelPosition='left' icon='edit' primary />
+                                    <Button content="Add Comment" labelPosition="left" icon="edit" primary />
                                 </Form>
                             </Comment.Group>
                         </div>
@@ -97,4 +84,3 @@ function GroupDetails({ match }) {
 }
 
 export default GroupDetails;
-
